bac715b4da5061fcf5cbe9d71e16ab633054b524
marzecm <marzec_m@yahoo.fr>
2019-12-08 02:14:49 +0100

    basic store setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Situation
+
+1. We have an empty angular app with eagerly loaded module called ```posts```
+2. We want to add @ngrx/store and @ngrx/store-devtools
+3. Get familiar with Redx DevTools: https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en
+4. Add minimal setup to start using store
+5. Check that state is properly initialized using store-devtools
+6. Display data from store in ```PostListComponent```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { PostsModule } from './posts/posts.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { postsReducer } from './posts/store/posts.reducer';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     PostsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({posts: postsReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 50,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
