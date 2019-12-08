e540cfe78479e0c370a3bb0a3122c3c289dd9898
marzecm <marzec_m@yahoo.fr>
2019-12-08 02:27:54 +0100

    extending initial state

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Situation
+# Situation (basic store setup)
 
 1. We have an empty angular app with eagerly loaded module called ```posts```
 2. We want to add @ngrx/store and @ngrx/store-devtools
@@ -6,3 +6,10 @@
 4. Add minimal setup to start using store
 5. Check that state is properly initialized using store-devtools
 6. Display data from store in ```PostListComponent```
+
+---
+
+# Situation (extending initial state)
+
+1. Create interface describing posts state. See ```State``` in posts.reducer.ts-node
+2. Create ```initialState``` and use it to initalize your store.
--- src/app/posts/store/posts.reducer.ts
+++ src/app/posts/store/posts.reducer.ts
@@ -1,3 +1,28 @@
-export function postsReducer(state = {}, action) {
+import { Post } from './../post.model';
+export interface State {
+  posts: Post[];
+  loading: boolean;
+}
+
+// tslint:disable: max-line-length
+const initialState: State = {
+  loading: false,
+  posts: [
+    {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    },
+  ],
+};
+
+export function postsReducer(state = initialState, action) {
   return state;
 }
